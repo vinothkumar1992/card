35104a48011b24253ae773a9449b2ee5b1d7dc46
vinoth92.r@gmail.com <vinoth@bdo.one>
2019-04-12 18:32:07 +0800

    Add Php Script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # card
+
+Please install wamp or Xampp server then copy the php script into htdocs. then change the url inside Services/personcard.js accordingly
--- src/services/personcard.js
+++ src/services/personcard.js
@@ -1,11 +1,9 @@
 import TestModel from "../component/card/test";
 
 const CardData = personCount => {
-  //let BaseURL = `http://127.0.0.1:8080/api/card.php?`;
   return new Promise((resolve, reject) => {
     fetch(`http://127.0.0.1:8080/api/card.php?personCount=` + personCount, {
       method: "GET"
-      //body: JSON.stringify(userData)
     })
       .then(response => response.json())
       .then(res => {
